2c749f1cdb3a408a6280455f7be5f49bdcfb6261
ApurvaMadtux <73700113+ApurvaMadtux@users.noreply.github.com>
2021-06-27 16:14:53 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,19 @@
+//java.lang is default pkg
+import java.util.Scanner;
+public class TestBasicProg {
+
+	public static void main(String[] args) {
+		
+		//output on console
+		
+		System.out.println("Welcome To JAVA");
+
+		int no1=10;
+		int no2=30;
+		int no3=no1+no2;
+		System.out.println(no3);
+		System.out.println("Add="+no3);
+		System.out.println(" Addition of "+no1+"+"+no2+"="+no3);
+		
+	}
+}
